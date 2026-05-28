e2ba9a06377debc6ebc4e0449766a0b809974bda
Aadhitya M B <aadhityamb@gmail.com>
2026-05-28 21:17:14 +0530

    fix: restore enableEdgeToEdge to fix black screen bug on Android 15

--- app/src/main/java/com/example/food_log/MainActivity.kt
+++ app/src/main/java/com/example/food_log/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.activity.enableEdgeToEdge
 import com.example.food_log.R
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Set up the custom MaterialToolbar as the Action Bar
